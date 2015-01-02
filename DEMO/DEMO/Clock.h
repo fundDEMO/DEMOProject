71a1e6e553d106aaf0324d49a51217c7dd76517e
sebanote <sebastianjpalacios@gmail.com>
2015-01-02 11:49:09 -0300

    desarrollo clase Clock.

--- DEMO/DEMO/Clock.h
+++ DEMO/DEMO/Clock.h
@@ -1,4 +1,4 @@
-// Clock.h: la clase representa un reloj que gobierna las funcionalidades de reproducci�n y de grabaci�n de un demo.
+// Clock.h: la clase representa un reloj que gobierna las funcionalidades de reproducción y de grabación de un demo.
 
 #ifndef CLOCK_H
 #define CLOCK_H
@@ -12,11 +12,17 @@ public:
 	~Clock();
 
 	float minuto(float minuto); // avanzar o retrasar reloj hasta un tiempo 'minuto' deseado.
+	void pauseClock(); // detiene el reloj instantáneamente.
+	void stopClock(); // detiene el reloj y lo vuelve a 0.
 	void startClock(); // inicia el conteo del tiempo desde el punto actual del reloj
+	
+	float getTiempoRep(); // devuelve el tiempo de reproducción válida en un momento dado.
 
-
+private:
+	float clockEn0; // indica el momento 0 de la reproducción. A partir de ahí se inicia el conteo.
+	float tiempoRep; // almacena el tiempo de reproducción válida.
+	bool pausado; // indica que el reloj está en pausa.
+	bool reseteado; // indica que el reloj se ha vuelto a 0 después de un stop.
 };
-
-
 
 #endif //!CLOCK_H
